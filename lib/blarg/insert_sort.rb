87d761935b8b4137a757f5ac242e78511e7a19eb
Anis Tayebali <anis.tayebali@hyfn.com>
2012-12-03 07:35:49 -0800

    New file added

---
+++ lib/blarg/insert_sort.rb
@@ -0,0 +1,21 @@
+module Blarg
+  class InsertSort
+    def initialize
+      p "Welcome to Insert Sort please enter some random numbers to sort"
+      @input = []
+      @input = $stdin.gets.chomp
+      run(@input)
+    end
+    
+    def run input
+      nums = []
+      input.split(" ").each {|n| nums << n.to_i}
+      p nums
+      p sort nums
+    end
+    
+    def sort nums
+      
+    end  
+  end
+end
