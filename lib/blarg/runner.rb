2ff8c6b810ae77ba5ce88a4c0f8dacdeebfc6441
Anis Tayebali <anis.tayebali@hyfn.com>
2012-12-03 09:14:54 -0800

    Made it a bit faster

--- lib/blarg/runner.rb
+++ lib/blarg/runner.rb
@@ -12,11 +12,12 @@ TO run ruby -I lib/ bin/blarg
      puts "Welcome to Blarg"
      puts "Type in LL for linked List"
      puts "Type in IS for InsertSort"
-     @choice = $stdin.gets.chomp
+     #@choice = $stdin.gets.chomp
+     @choice = "IS"
    end
    
    def run 
-     LinkedList.new if @choice == "LL"
+     #LinkedList.new if @choice == "LL"
      InsertSort.new if @choice == 'IS'
    end
     
