5f981fa8fdd882e3e0261427592e4fc907a9a85f
Anis Tayebali <anis.tayebali@hyfn.com>
2012-12-03 07:34:35 -0800

    Committing start of the madness

--- lib/blarg/linked_list.rb
+++ lib/blarg/linked_list.rb
@@ -1,7 +1,19 @@
 module Blarg
   class LinkedList
     def initialize
-      p "BANG in LL"
+     puts "Welcome to Linked List"
+     puts "Please enter Y to continue...."
+     a = $stdin.gets.chomp
+     if a == 'Y'
+       run
+     else
+       p "in Run yippie!"
+      end      
     end
+    
+    def run
+      p "Running mad hard"
+    end
+    
   end
 end
--- lib/blarg/runner.rb
+++ lib/blarg/runner.rb
@@ -1,5 +1,6 @@
 #require 'optparse'
 require_relative 'linked_list'
+require_relative 'insert_sort'
 
 module Blarg
   class Runner
@@ -7,13 +8,16 @@ module Blarg
 =begin
 TO run ruby -I lib/ bin/blarg 
 =end  
-   def initialize(argv)
-     @options = argv.first
-     p @options
+   def initialize
+     puts "Welcome to Blarg"
+     puts "Type in LL for linked List"
+     puts "Type in IS for InsertSort"
+     @choice = $stdin.gets.chomp
    end
    
    def run 
-     l = LinkedList.new if @options == "LL"
+     LinkedList.new if @choice == "LL"
+     InsertSort.new if @choice == 'IS'
    end
     
   end
