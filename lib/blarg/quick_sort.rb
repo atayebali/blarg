2e88e4446c9c53487d4db4bcb9a7675ce4b33fab
Anis Tayebali <anis.tayebali@hyfn.com>
2013-01-10 07:17:25 -0800

    Fixed the last broken attempt

--- lib/blarg/quick_sort.rb
+++ lib/blarg/quick_sort.rb
@@ -3,11 +3,21 @@ module Blarg
     def initialize(input = nil)
       print <<-DOC
 
-      Use of Partitioning to cut the size of data down and then sorting based on one element (pivot)
+      Use of Partitioning to cut the size of data down and then sorting based on one element (pivot).
+
+      Similar idea to Merge sort but we cut down on the space requirement.
 
       Effectively, elements less than pivot will go on one side and elements greater than pivot will go on the another.
 
+      The key idea here is maintaining three different regions of the array (done in place to cut down on space requirement).
+       - Area smaller than pivot
+       - Area larger than pivot
+       - Unexplored area.
+
       Interestingly the pivot ends up in the correct sorted order after the first partition step.
+
+      Best Case: O(n lg n) n partitioning steps.  Recursive tree with lg n levels.
+      Worst Case: O(n^2)
 
       DOC
       @input = []
@@ -16,49 +26,44 @@ module Blarg
 
     def sort
       data = @input
-
       quick_sort(data, 0, data.count-1)
+      data
     end
 
-=begin
 
-
-=end
     def quick_sort(data, low, high)
       if low < high
-        pivot_random = rand(data.count-1)
+        pivot_random = rand low..high
         pivot = partition(data, low, high, pivot_random)
         quick_sort(data, low, pivot - 1 )
         quick_sort(data, pivot + 1, high )
       end
-      return data
     end
 
 
     def partition(data, low, high, pivot)
       pivot_value = data[pivot]
-      temp = data.last
 
-      data[data.count-1] = pivot_value
-      data[pivot] = temp
+      swap(data,high, pivot)
+
       store_index = low
 
       low.upto(high-1) do |i|
         if data[i] < pivot_value
-          temp = data[i]
-          data[i] = data[store_index]
-          data[store_index] = temp
+          swap data, i, store_index
           store_index += 1
         end
       end
-      temp = data[store_index]
-      data[store_index] = data[high]
-      data[high] = temp
+      swap data, high, store_index
       return store_index
     end
 
-
-
+    def swap(data, id1, id2)
+      temp = data[id1]
+      data[id1] = data[id2]
+      data[id2] = temp
+      data
+    end
   end
 end
 
